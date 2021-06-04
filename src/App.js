81d1a4c0481c05dbe220bd36a37df84774d97e02
nlom0218 <nlom0218@naver.com>
2021-06-04 23:02:35 +0900

    Router SetUp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
+  const isLoggedIn = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route><NotFound /></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/screens/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+  return (<div>
+    You need Login
+  </div>);
+}
+
+export default Login;
